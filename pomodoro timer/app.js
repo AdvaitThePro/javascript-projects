bdd10591932218d106d45f433aa65ce7591cbab6
Advait Conty <advaitthepro@outlook.com>
2022-10-18 13:40:41 +0800

    added pomodoro timer template

---
+++ pomodoro timer/app.js
@@ -0,0 +1,47 @@
+//App logic in JS goes here
+// The data/time we want to countdown to
+
+const presetTimer = {
+
+};
+
+const Timer = {
+
+};
+
+// Setup
+
+
+function SetTime(timeMins) {
+
+}
+
+function Onclick_PomodoroBtn() {
+
+}
+
+function Onclick_ShortBreakBtn() {
+
+}
+
+function Onclick_LongBreakBtn() {
+    
+}
+
+function UpdateResetTime() {
+
+}
+
+function UpdateTimeDisplay() {
+
+}
+
+function ToggleTimer() {
+    interval = setInterval(function () {
+
+    }, 1000)    // delay for each interval in milliseconds
+}
+
+function Reset() {
+
+}
