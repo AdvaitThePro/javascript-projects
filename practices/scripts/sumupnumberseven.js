3d0ce1532c10b2878a617f55578af3425f62dfb7
Advait Conty <advaitthepro@outlook.com>
2022-10-18 13:39:04 +0800

    all practice projects done

--- practices/scripts/sumupnumberseven.js
+++ practices/scripts/sumupnumberseven.js
@@ -1,6 +1,5 @@
 let firstnum = parseInt(prompt("What is your first number?"));
 let lastnum = parseInt(prompt("What is your last number?"));
 
-let answer = ((firstnum + lastnum) * (lastnum - firstnum + 1) / 2);
-
+let answer = (((firstnum + lastnum) * (lastnum - firstnum + 1) / 2) / 2);
 console.log(answer);
