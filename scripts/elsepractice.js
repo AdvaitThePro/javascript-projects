3f40652c09140496de6d9bc6fc38dae30464e52a
Advait Conty <advaitthepro@outlook.com>
2022-10-18 11:30:02 +0800

    added multiple files and moved to index

---
+++ scripts/elsepractice.js
@@ -0,0 +1,13 @@
+let grade = prompt("Enter Your Grade:"); // gives a prompt in browser (similar to input)
+if (grade >= 90) { // use brackets for conditions
+    console.log("You got a A");
+} else if (grade >= 80) { // else if = elif
+    console.log("You got a B");
+} else if (grade >= 70) {
+    console.log("You got a C");
+} else if (grade >= 60) { 
+    console.log("You got a D");
+} else {
+    console.log("You got a F");
+}
+
---
+++ scripts/forloopspractice.js
@@ -0,0 +1,3 @@
+for (i = 1; i <= 5; i++) { // Use semicolons to seperate the main 3 statements
+    console.log("I love Javascript"); // the equivelent to python "print"
+} // end
