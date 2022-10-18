9fc747c6a8dccf4babcd4e94683e423680532b02
Advait Conty <advaitthepro@outlook.com>
2022-10-18 11:44:00 +0800

    added 3rd practice

---
+++ practices/scripts/sumupnumbers.js
@@ -0,0 +1,6 @@
+let firstnum = parseInt(prompt("What is your first number?"));
+let lastnum = parseInt(prompt("What is your last number?"));
+
+let answer = ((firstnum + lastnum) * (lastnum - firstnum + 1) / 2);
+
+console.log(answer)
